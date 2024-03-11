9e5b10079b4b8e9b5f512bdfaccf32223e00a9ce
DracoR22 <rdraco039@gmail.com>
2024-03-10 23:10:53 -0500

    quick fix

--- src/app/(platform)/dashboard/page.tsx
+++ src/app/(platform)/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import DashboardSetup from "@/components/dashboard/dashboard-setup"
 import { currentUser } from "@/lib/auth/get-server-session"
+import { getUserSubscriptionPlan } from "@/lib/stripe/stripe"
 import { getFirstCampaign } from "@/server/actions/reddit-actions"
 import { redirect } from "next/navigation"
 
@@ -12,11 +13,12 @@ const DashboardPage = async () => {
   }
 
   const project = await getFirstCampaign(user.id)
+  const subscriptionPlan = await getUserSubscriptionPlan()
 
     if (!project) {
       return (
         <div className="h-screen w-screen flex justify-center items-center">
-           <DashboardSetup/>
+           <DashboardSetup subscriptionPlan={subscriptionPlan}/>
         </div>
       )
     }
