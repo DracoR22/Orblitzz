34364339ea8ca238bef7a9562ea7a8a8f07ee076
DracoR22 <rdraco039@gmail.com>
2024-03-31 22:24:12 -0500

    landing page button fix

--- src/components/landing-page/navbar.tsx
+++ src/components/landing-page/navbar.tsx
@@ -59,7 +59,7 @@ const Navbar = () => {
           <div className="w-full">
           <Link href="/login" className="relative inline-flex w-[100px] h-10 overflow-hidden rounded-md p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#0074FF_0%,#00C3FF_50%,#0074FF_100%)]" />
-          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-black px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md dark:bg-neutral-900 bg-background px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
             Log In
           </span>
         </Link>
