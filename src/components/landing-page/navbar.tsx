3559fa743af66dfcc3c745ba2a24f440471997c1
DracoR22 <rdraco039@gmail.com>
2024-04-02 00:35:41 -0500

    features section completed

--- src/components/landing-page/navbar.tsx
+++ src/components/landing-page/navbar.tsx
@@ -9,7 +9,8 @@ import Image from "next/image"
 import { poppins } from "../fonts/fonts"
 import { HoverBorderGradient } from "../global/animations/hover-border-gradient-animation"
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet"
-import { MenuIcon } from "lucide-react"
+import { BarChart, BotIcon, Clock1Icon, Link2, MenuIcon, ReplyIcon, SquarePenIcon } from "lucide-react"
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuList, NavigationMenuTrigger } from "../ui/navigation-menu"
 
 const Navbar = () => {
 
@@ -18,30 +19,89 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className={cn("hidden z-[99999] bg-transparent fixed top-0 md:flex w-full p-4 transition-all md:px-20" ,
+      <nav className={cn("hidden z-[99999] bg-transparent fixed top-0 md:flex w-full p-2 transition-all md:px-20 " ,
     scrolled && "border-b shadow-sm bg-white/75 dark:bg-[#1e1e1e] dark:bg-opacity-50 backdrop-blur-md")}>
       {/* LOGO */}
        <Link href={'/'} className="hidden md:flex items-center cursor-pointer">
-         <Image draggable="false" quality={100} src="/logo-bg.png" height={65} width={65} alt="Logo" className="rounded-full object-cover"/>
+         <Image draggable="false" quality={100} src="/logo-bg.png" height={55} width={55} alt="Logo" className="rounded-full object-cover"/>
          <p className={cn("font-semibold text-2xl transition ml-2", poppins.className)}>
            Orblitzz
          </p>
        </Link>
 
        {/* SIGN IN AND UP */}
-       <div className="md:ml-auto md:justify-end justify-between flex items-center gap-x-2">
-         <div>
-             <Button asChild variant='ghost' className="font-semibold">
-              <Link href={'#pricing'}>
-                  Pricing
-              </Link>
-             </Button>
-         </div>
-        
+       <div className="md:ml-auto md:justify-end justify-between flex items-center gap-x-2">   
           <div>
-             <Button variant='ghost' className="font-semibold" onClick={() => router.push('/log-in')}>
-               Features
-             </Button>
+             <NavigationMenu>
+               <NavigationMenuList>
+                 <NavigationMenuItem>
+                   <NavigationMenuTrigger className="font-semibold">
+                       Features
+                   </NavigationMenuTrigger>
+                   <NavigationMenuContent className="dark:bg-[#1e1e1e] bg-white">
+                    <div className="grid grid-cols-2 gap-x-6 w-[430px] p-4">
+                       <div>
+                        <div className="flex items-center gap-x-3">
+                          <ReplyIcon className="w-5 h-5"/>
+                          <span className="text-sm font-semibold">AI Reddit Replies</span>
+                          
+                        </div>
+                          <p className="text-sm text-muted-foreground mt-3">
+                            Automatic Reddit replies
+                          </p>
+
+                          <div className="flex items-center gap-x-3 mt-4">
+                          <BarChart className="w-5 h-5"/>
+                          <span className="text-sm font-semibold">Advanced Analytics</span>
+                          
+                        </div>
+                          <p className="text-sm text-muted-foreground mt-3">
+                            Powerful real time analytics
+                          </p>
+
+                          <div className="flex items-center gap-x-3 mt-4">
+                          <Link2 className="w-5 h-5"/>
+                          <span className="text-sm font-semibold">Personalization</span>
+                          
+                        </div>
+                          <p className="text-sm text-muted-foreground mt-3">
+                            Personalize the AI tone
+                          </p>
+                       </div>
+
+                       <div>
+                        <div className="flex items-center gap-x-3">
+                          <BotIcon className="w-5 h-5"/>
+                          <span className="text-sm font-semibold">Human Level Replies</span>
+                          
+                        </div>
+                          <p className="text-sm text-muted-foreground mt-3">
+                            Powered by GPT-4 
+                          </p>
+
+                          <div className="flex items-center gap-x-3 mt-4">
+                          <Clock1Icon className="w-5 h-5"/>
+                          <span className="text-sm font-semibold">Fast Setup</span>
+                          
+                        </div>
+                          <p className="text-sm text-muted-foreground mt-3">
+                            It only takes one minute!
+                          </p>
+
+                          <div className="flex items-center gap-x-3 mt-4">
+                          <SquarePenIcon className="w-5 h-5"/>
+                          <span className="text-sm font-semibold">AI Text Editor</span>
+                          
+                        </div>
+                          <p className="text-sm text-muted-foreground mt-3">
+                            Write posts with AI help <span className="text-xs font-semibold">(Comming Soon)</span>
+                          </p>
+                       </div>
+                    </div>
+                   </NavigationMenuContent>
+                 </NavigationMenuItem>
+               </NavigationMenuList>
+             </NavigationMenu>
           </div>
 
           <div>
@@ -49,6 +109,14 @@ const Navbar = () => {
                How it Works?
              </Button>
           </div>
+
+          <div>
+             <Button asChild variant='ghost' className="font-semibold">
+              <Link href={'#pricing'}>
+                  Pricing
+              </Link>
+             </Button>
+         </div>
 
           <div>
              <Button variant='ghost' className="font-semibold" onClick={() => router.push('/')}>
